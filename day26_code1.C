f78098862f8ccde812def6d8cb6bafa3a165c986
sharma-kanishk <mazuko1661@gmail.com>
2026-09-05 17:44:32 +0530

    Add files via upload

---
+++ day26_code1.C
@@ -0,0 +1,22 @@
+/*Write a program to print the following pattern:
+    5
+   4 5
+  3 4 5
+ 2 3 4 5
+1 2 3 4 5
+*/
+#include <stdio.h>
+int main (){
+      int i , j;
+      
+      for (i = 5; i >= 1; i--) {
+          for (j = 1; j < i; j++) {
+              printf(" ");
+          }
+          for (j = i; j <= 5; j++) {
+              printf(" %d",j);
+          }
+          printf("\n");
+      }
+      return 0;
+}
