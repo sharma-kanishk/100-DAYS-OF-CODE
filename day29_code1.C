a084fc36c722c645a4f0d494145eb15438e2ffa6
sharma-kanishk <mazuko1661@gmail.com>
2026-09-08 21:10:33 +0530

    Add files via upload

---
+++ day29_code1.C
@@ -0,0 +1,20 @@
+//Find the sum of array elements.
+#include <stdio.h>
+int main (){
+       int i , n;
+       int sum = 0;
+       printf("enter the number of elements:\n");
+       scanf("%d",&n);
+       
+       int arr[n];
+       printf("enter the elements:\n");
+       for (i = 0 ; i < n ; i++) {
+           scanf("%d",&arr[i]);
+       } 
+       
+       for (i = 0 ; i < n ; i++) {
+           sum = sum + arr[i];
+       }
+       printf("the sum of array elements is:%d", sum);
+       return 0;
+}
