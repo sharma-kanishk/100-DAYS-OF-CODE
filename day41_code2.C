43f941fdbefaff588890ee8d14d394512f810b49
sharma-kanishk <mazuko1661@gmail.com>
2026-09-19 22:12:55 +0530

    Add files via upload

---
+++ day41_code2.C
@@ -0,0 +1,18 @@
+//Print each character of a string on a new line.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != '\n') {
+            printf("%c\n", str[i]);
+        }
+    }
+
+    return 0;
+}
